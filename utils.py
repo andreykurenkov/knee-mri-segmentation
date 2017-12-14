e4abf8428c84189142bfbb37c16ca3dc30bb9bef
Andrey Kurenkov <andreyvkurenkov@gmail.com>
2017-12-14 09:39:31 -0800

    A few additions for evaluation

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import numpy as np
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
-from keras.callbacks import *
 from keras.losses import *
 from keras.preprocessing.image import *
 from os.path import isfile
@@ -31,7 +30,7 @@ def show_samples(x, y, num):
         plt.subplot(1,2,2)
         img = y[rnd[i]]
         plt.axis('off')
-        plt.imshow(img)
+        plt.imshow(np.repeat(img,3,2))
         plt.show()
 
 def shuffle(x, y):
@@ -51,123 +50,6 @@ def split(x, y, tr_size=0.8, va_size=0.1):
     x_te = x[va_start:]
     y_te = y[va_start:]
     return x_tr, y_tr, x_va, y_va, x_te, y_te
-
-def augment(x, y, h_shift=[], v_flip=False, h_flip=False, rot90=False, edge_mode='minimum'):
-    assert(len(x.shape) == 4)
-    seg = False if len(y.shape) <= 2 else True
-    if h_shift and h_shift != 0 and len(h_shift) != 0:
-        tmp_x, tmp_y = [], []
-        for shft in h_shift:
-            if shft > 0:
-                tmp = np.lib.pad(x[:, :, :-shft], ((0,0), (0,0), (shft,0), (0,0)), edge_mode)
-                tmp_x.append(tmp)
-                if seg:
-                    tmp = np.lib.pad(y[:, :, :-shft], ((0,0), (0,0), (shft,0), (0,0)), edge_mode)
-                else:
-                    tmp = y
-                tmp_y.append(tmp)
-            else:
-                tmp = np.lib.pad(x[:, :, -shft:], ((0,0), (0,0), (0,-shft), (0,0)), edge_mode)
-                tmp_x.append(tmp)
-                if seg:
-                    tmp = np.lib.pad(y[:, :, -shft:], ((0,0), (0,0), (0,-shft), (0,0)), edge_mode)
-                else:
-                    tmp = y
-                tmp_y.append(tmp)
-        x = np.concatenate((x, np.concatenate(tmp_x)))
-        y = np.concatenate((y, np.concatenate(tmp_y)))
-    if v_flip:
-        tmp = np.flip(x, axis=1)
-        x = np.concatenate((x, tmp))
-        if seg:
-            tmp = np.flip(y, axis=1)
-            y = np.concatenate((y, tmp))
-        else:
-            y = np.concatenate((y, y))
-    if h_flip:
-        tmp = np.flip(x, axis=2)
-        x = np.concatenate((x, tmp))
-        if seg:
-            tmp = np.flip(y, axis=2)
-            y = np.concatenate((y, tmp))
-        else:
-            y = np.concatenate((y, y))
-    if rot90:
-        tmp = np.rot90(x, axes=(1,2))
-        x = np.concatenate((x, tmp))
-        if seg:
-            tmp = np.rot90(y, axes=(1,2))
-            y = np.concatenate((y, tmp))
-        else:
-            y = np.concatenate((y, y))
-    return x, y
-
-def to_2d(x):
-    assert len(x.shape) == 5 # Shape: (#, Z, Y, X, C)
-    return np.reshape(x, (x.shape[0]*x.shape[1], x.shape[2], x.shape[3], x.shape[4]))
-
-def to_3d(imgs, z):
-    assert len(imgs.shape) == 4 # Shape: (#, Y, X, C)
-    return np.reshape(imgs, (imgs.shape[0] / z, z, imgs.shape[1], imgs.shape[2], imgs.shape[3]))
-
-def get_crop_area(img, threshold=0):
-    y_arr = np.where(img.sum(axis=0) > threshold)[0]
-    size = y_arr[-1] - y_arr[0] + 1
-    y = y_arr[0]
-    x_arr = np.where(img.sum(axis=0).sum(axis=0) > threshold)[0]
-    x = (x_arr[0] + x_arr[-1]) // 2 - size // 2
-    return y, x, size
-
-def erode(imgs, amount=3):
-    imgs = imgs.sum(axis=-1)
-    for i in range(len(imgs)):
-        imgs[i] = mo.erosion(imgs[i], mo.square(amount))
-    return imgs[..., np.newaxis]
-
-def add_noise(imgs, amount=3):
-    imgs = imgs.sum(axis=-1)
-    for i in range(len(imgs)):
-        if i % 2 == 0:
-            imgs[i] = mo.dilation(imgs[i], mo.square(amount))
-        else:
-            imgs[i] = mo.erosion(imgs[i], mo.square(amount))
-    return imgs[..., np.newaxis]
-            
-
-# Label helper functions
-
-def to_classes(y, start, end, step=1):
-    age_range = end - start
-    num_classes = int(round(age_range / step))
-    labels = np.zeros((len(y), num_classes))
-    idx = (y - start) / step
-    for i in range(len(idx)):
-        labels[i, int(idx[i])] = 1
-    return labels
-
-def y_center(img, smooth=20, crop=100):
-    # Get Sum of y-axis values
-    y = img.sum(axis=-1).sum(axis=-1).sum(axis=0)
-    # Smooth the values and apply the crop region
-    y_vec = np.convolve(y, np.ones(smooth)/smooth, mode='same')[crop:-crop]
-    # 2nd derivative of min will be max - get its index
-    return np.gradient(np.gradient(y_vec)).argmax() + crop
-
-def lengthen(y, factor):
-    arr = []
-    for el in y:
-        for i in range(factor):
-            arr.append(el)
-    return np.array(arr)
-
-def shorten(y, factor):
-    arr = []
-    for i in range(0, len(y), factor):
-        arr.append(y[i])
-    return np.array(arr)
-
-def normalize(x, mean, std):
-    return (x - x.mean()) / x.std()
 
 def read_image(path, size=None, norm=False, grey=False):
     fd_img = open(path, 'rb')
@@ -190,7 +72,7 @@ def load_data(path, size=(24,224,224), norm=False):
         img = read_image(x_path, size=size, norm=norm)
         x.append(img)
         y_path = x_path.replace('img','mask')
-        img = read_image(y_path, size=size, norm=norm, grey=True)
+        img = read_image(y_path, size=size, norm=norm, grey=True)>0.5
         y.append(img)
     x = np.array(x)
     y = np.array(y)
